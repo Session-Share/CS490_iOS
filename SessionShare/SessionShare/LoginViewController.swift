077f38c944cbd25640e61d4b2c41e17429e86dfd
SidMad <siddharth.madapati@gmail.com>
2022-04-09 17:37:50 -0400

    Configured Spotify Framework

--- SessionShare/SessionShare/LoginViewController.swift
+++ SessionShare/SessionShare/LoginViewController.swift
@@ -6,22 +6,30 @@
 //
 
 import UIKit
-
 class LoginViewController: UIViewController {
 
+
+    let SpotifyClientID = "57ecd291e22142faab9a2841c92d9236"
+    let SpotifyRedirectURL = URL(string: "https://localhost:8080/cs490-sessionshare")!
+    lazy var configuration = SPTConfiguration(
+      clientID: SpotifyClientID,
+      redirectURL: SpotifyRedirectURL
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
-    
     @IBAction func loginToSpotify(_ sender: Any) {
         
         print("Login");
         // TODO: Login to spotify and get backend
         // Maybe segue to a new storyboard where they can enter the details
     }
+    
     
 }
 
---
+++ SessionShare/SessionShare/SessionShare-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  SessionShare-Bridging-Header.h
+//  SessionShare
+//
+//  Created by Siddharth Madapati on 9/4/22.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
