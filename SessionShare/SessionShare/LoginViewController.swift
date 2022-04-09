cb94e19bff1fdf6df2d4415bec6feae6f4339185
SidMad <siddharth.madapati@gmail.com>
2022-04-09 16:47:33 -0400

    Created xcode project,added Spotify frameworks and made basic login screen

---
+++ SessionShare/SessionShare/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SessionShare
+//
+//  Created by Siddharth Madapati on 9/4/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    @IBAction func loginToSpotify(_ sender: Any) {
+        
+        print("Login");
+        // TODO: Login to spotify and get backend
+        // Maybe segue to a new storyboard where they can enter the details
+    }
+    
+}
+
